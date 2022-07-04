6eabb11659a95c5beed72861ea0a8b34ff58f352
ARETEE <73266671+ARETEE0@users.noreply.github.com>
2022-07-04 11:06:24 +0545

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+      ),
+      designSize: const Size(390, 844),
+    );
+  }
+}
